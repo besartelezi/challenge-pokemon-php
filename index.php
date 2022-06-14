d487006c781d05625eaa763f31d77ccd1b71b990
Besart Elezi <besart.elezi@hotmail.com>
2022-06-14 17:00:09 +0200

    added evolutionary line in text

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <!-- Getting all the needed information from the API -->
-<?php $pokemonAPI = "https://pokeapi.co/api/v2/"; ?>
-
+<?php $searchedPokemon = "https://pokeapi.co/api/v2/pokemon/"; ?>
+<?php $pokemonSpecies = "https://pokeapi.co/api/v2/pokemon-species/"; ?>
+<!-- the php if(isset()), according to me, will make something visible once the user has input something -->
 <?php if(isset($_GET['pokemonName'])): ?>
 
     <?php
     //Adds the searched ID or name of the Pokémon to the API URL
     //In JS, you add something to a string by adding a '+' sign between strings, in PHP, you do this like how I did it here
-    $searchedPokemon = "https://pokeapi.co/api/v2/pokemon/";
     $searchedPokemon .= $_GET["pokemonName"];
+
+    $pokemonSpecies .= $_GET["pokemonName"];
     ?>
 
     <?php
     //Get data of searched Pokémon
     $getPokemonData = file_get_contents($searchedPokemon);
+    //Set data into an array
     $getPokemonDataArray = json_decode($getPokemonData);
     //Get image of searched Pokémon
     $pokemonImage = $getPokemonDataArray->sprites->other->home->front_default;
@@ -36,36 +39,61 @@
                 <input type="submit" name="searchPokemonButton" id="searchPokemonButton"
                        class="button" value="Search Pokémon!" />
         </section>
-
-
         <div class ="IDNumber" id="IDNumber"> ID Number of Pokémon:</div>
         <?php if(isset($_GET['pokemonName'])): ?>
-
             <?php
             //show ID of searched Pokémon
             echo $getPokemonDataArray->id; ?>
         <?php endif; ?>
 
-
-
         <section class="ScreenWrapper">
             <div class="ScreenBackground">
                 <div class="PokemonImage">
+                    <!-- Shows image of Pokémon -->
                     <?php if(isset($_GET['pokemonName'])): ?>
                         <img src="<?php echo $pokemonImage;?>" alt="The Pokémon that the user has searched for" id="PokemonPicture">
                     <?php endif; ?>
-
                 </div>
             </div>
             <div class ="Name" id="Name">Name of Pokémon:</div>
+            <!-- Shows name of Pokémon -->
             <?php if(isset($_GET['pokemonName'])): ?>
                 <?php
                 echo $getPokemonDataArray->name;
                 ?>
             <?php endif; ?>
-
             <div class="EvolutionRow" id="EvolutionRow">
+                <?php if(isset($_GET['pokemonName'])): ?>
 
+                    <?php
+                    //Get data of the species searched Pokémon
+                    $getSpeciesData = file_get_contents($pokemonSpecies);
+                    //Set data into an array
+                    $getSpeciesDataArray = json_decode($getSpeciesData);
+
+                    $getEvolutionChainURL = $getSpeciesDataArray->evolution_chain->url;
+                    $getEvolutionsData = file_get_contents($getEvolutionChainURL);
+                    $getEvolutions = json_decode($getEvolutionsData);
+
+                    ?>
+
+                    <?php
+                var_dump($getEvolutions->chain->species->name);
+                    if ($getEvolutions->chain->evolves_to){
+                        for ($i = 0; $i < count($getEvolutions->chain->evolves_to); $i++){
+                            $evolution = $getEvolutions->chain->evolves_to[$i]->species->name;
+                            var_dump($evolution);
+                            if ($getEvolutions->chain->evolves_to[$i]->evolves_to){
+                                for ($j = 0; $j < count($getEvolutions->chain->evolves_to[$i]->evolves_to); $j++){
+                                    $secondEvolution = $getEvolutions->chain->evolves_to[$i]->evolves_to[$j]->species->name;
+                                    var_dump($secondEvolution);
+                                }
+                            }
+                        }
+                    }
+                    ?>
+
+                <?php endif; ?>
             </div>
         </section>
         <section class="BottomSide">
